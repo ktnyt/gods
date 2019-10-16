1271aeeda98b40c4431b68748bb684ab572dea35
Kotone Itaya <kotone@sfc.keio.ac.jp>
2019-10-16 14:13:36 +0900

    Add Iter method

--- ordered.go
+++ ordered.go
@@ -23,6 +23,11 @@ func (dict Ordered) Len() int {
 	return len(dict)
 }
 
+// Iter returns the underlying slice.
+func (dict Ordered) Iter() []Entry {
+	return []Entry(dict)
+}
+
 // Indices lists the indices for entries matching the filter.
 func (dict Ordered) Indices(filter func(Entry) bool) []int {
 	is := make([]int, 0)
